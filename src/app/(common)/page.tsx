aff68640b3c7f81cfc9393c4e1c85c521528a8eb
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-05-04 05:23:46 +0600

    asdfasf

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,3 +1,5 @@
+import HappenBanner from "@/components/cards/HappenBanner/HappenBanner";
+import ContactPage from "@/components/ContactUs/ContactUs";
 import Banner from "@/components/pages/home/banner/Banner";
 import PortfolioSwiper from "@/components/pages/home/Our-Portfolio/Portfolio";
 import OurService from "@/components/pages/home/OurService/OurService";
@@ -5,7 +7,7 @@ import TrustedByHundreds from "@/components/pages/home/TrustedByHundreds/Trusted
 import YourTrustedPartner from "@/components/pages/home/YourTrustedPartner/YourTrustedPartner";
 import BlogComponent from "@/components/shared/blog/BlogComponent";
 import ImproveProperty from "@/components/shared/ImproveProperty/ImproveProperty";
-import TrustedTrader from "@/components/shared/TrustedTrader/TrustedTrader";
+// import TrustedTrader from "@/components/shared/TrustedTrader/TrustedTrader";
 import TestimonialSection from "@/components/Testimonial/Testimonial";
 import { Metadata } from "next";
 
